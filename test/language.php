dd869ca62d402a8149a0b4f9feeaeafdfd1ccd4d
Abed El Rahmane Jaafir <abed.r.jaafir@gmail.com>
2024-06-10 21:06:48 +0300

    Delete test directory

--- test/language.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_GET['lang'])) {
-    $lang = $_GET['lang'];
-    $_SESSION['lang'] = $lang;
-    header("Location: " . $_SERVER['HTTP_REFERER']);
-    exit();
-}
-?>
